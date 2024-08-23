922820b06460e1e4bbb205c49f6955bbae411979
Salvatore Assennato <137400679+Salvatore-12@users.noreply.github.com>
2024-08-23 17:39:23 +0200

    aggiunto il componente Homepage

---
+++ src/Homepage.jsx
@@ -0,0 +1,13 @@
+const Homepage = ()=>{
+    return(<>
+    <h3>Information</h3>
+     <div className="MyBiografy">
+        <img src="" alt="" />
+        <p>Ciao sono sono Salvatore Assennato,sono un full-stack-developer,ed sono disponibile a lavora da subito,dove darò il massimo del mio contributo ,nei vari progetti che mi dedicherò </p>
+        </div>
+     </>
+
+)
+}
+
+export default Homepage
