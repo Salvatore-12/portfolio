cf2686289e7dad19f9fe409a0732011513aa5a09
Salvatore Assennato <137400679+Salvatore-12@users.noreply.github.com>
2024-08-30 10:45:47 +0200

    sistemazione paragrafo e stile css

--- src/Homepage.jsx
+++ src/Homepage.jsx
@@ -54,7 +54,11 @@ const Homepage = ()=>{
       <div className="MyBiografy">
       <h3 className='text-black'>Information:</h3> 
         <img src= {fotoProfilo} alt="immagine profilo" className='imageProfile' />
-        <p>Ciao sono Salvatore Assennato,sono un full-stack-developer,ed sono disponibile a lavorare sia da remoto che ibrido,ed lavorare in gruppo.Amo fare programmazione,sono una persona ordinata ai dettagli e orientato alla risoluzione dei problemi.Dove darò il massimo del mio contributo,nei vari progetti che mi dedicherò. </p>
+        <p>Ciao sono Salvatore Assennato,
+          sono un full-stack-developer,
+          ed sono disponibile a lavorare sia da remoto che ibrido,
+          ed lavorare in gruppo.Amo fare programmazione,
+          sono una persona ordinata ai dettagli e orientato alla risoluzione dei problemi.Dove darò il massimo del mio contributo,nei vari progetti che mi dedicherò. </p>
       </div> 
 
       <div className="Skills">
